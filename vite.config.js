ed96dc437ff4d60b6735f5779ddd4bebcc1a7978
Abhipray Dumka <dumkaabhipray@gmail.com>
2024-03-23 15:58:19 +0530

    try

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dotenv from 'dotenv'
+import { VitePWA } from 'vite-plugin-pwa'
 
-dotenv.config()
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [VitePWA({ registerType: 'autoUpdate' })]
 })
