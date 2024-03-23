9018c0d6598019cf6cba479dcb4f629b190aec87
Abhipray Dumka <dumkaabhipray@gmail.com>
2024-03-23 16:17:19 +0530

    pwa final deployment

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router'
 import store from './store/index'
 import Vue3Lottie from 'vue3-lottie'
 import './index.css'
-import './registerServiceWorker'
 const app = createApp(App)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
 app.use(router)
 app.use(store)
 app.use(Vue3Lottie)
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,106 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv'
 
+dotenv.config()
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({ registerType: 'autoUpdate' })]
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'Shayrana',
+        short_name: 'Shay',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#CC6FDC',
+        icons: [
+          {
+            src: './src/assets/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '152x152',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '120x120',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '167x167',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '16x16',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '48x48',
+            type: 'image/png'
+          },
+          {
+            src: './src/assets/logo.png',
+            sizes: '144x144',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
