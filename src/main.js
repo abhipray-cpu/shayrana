11de93274efd2513fd612a9cd77c50b1eb03b204
Abhipray Dumka <dumkaabhipray@gmail.com>
2024-03-23 15:15:45 +0530

    pwa is done

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store/index'
 import Vue3Lottie from 'vue3-lottie'
 import './index.css'
+import './registerServiceWorker'
 const app = createApp(App)
 app.use(router)
 app.use(store)
